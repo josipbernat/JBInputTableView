9cd572c5f2c5823ecaaca16957594758d8e36a49
Josip Bernat <josip.bernat@clover-studio.com>
2015-02-25 16:15:54 +0100

    Initial Commit

---
+++ JBInputTableView/AppDelegate.h
@@ -0,0 +1,17 @@
+//
+//  AppDelegate.h
+//  JBInputTableView
+//
+//  Created by Josip Bernat on 2/25/15.
+//  Copyright (c) 2015 Clover-Studio. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface AppDelegate : UIResponder <UIApplicationDelegate>
+
+@property (strong, nonatomic) UIWindow *window;
+
+
+@end
+
